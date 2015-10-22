f4165a929e077942f751f417190d61631af77ac5
imane <imane@imane-virtual-machine.(none)>
2015-10-22 20:22:23 +0800

    add download file function

--- operate/application/helpers/files_helper.php
+++ operate/application/helpers/files_helper.php
@@ -53,6 +53,24 @@ class Files{
 		header('Content-Length: ' . strlen($content));
 		echo $content;
 	}
+
+	function downloadFile($filename)
+	{
+		if (!file_exists($filename))
+			return;
+
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($filename));
+		ob_clean();
+		flush();
+		readfile($filename);
+	}
 }
 
 ?>
