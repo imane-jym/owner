3661c6bb0b0e125038d15800f41750627b04fbe3
imane <imane@imane-virtual-machine.(none)>
2016-04-07 17:28:08 +0800

    360 cdn visit fail

--- operate/application/config/config.php
+++ operate/application/config/config.php
@@ -1,14 +1,14 @@
 <?php 
 
-$config['base_url'] = 'http://172.16.1.158/opt'; // Base URL including trailing slash (e.g. http://localhost/)
+$config['base_url'] = 'http://172.16.0.89/opt'; // Base URL including trailing slash (e.g. http://localhost/)
 
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
 $config['db_host'] = '127.0.0.1'; // Database host (e.g. localhost)
-$config['db_name'] = 'operate'; // Database name
-$config['db_username'] = 'root'; // Database username
-$config['db_password'] = 'imane'; // Database password
+$config['db_name'] = 'web_operate'; // Database name
+$config['db_username'] = '3d'; // Database username
+$config['db_password'] = '123456'; // Database password
 
 $config['login_user'] = 'imane'; //user name http auth use
 $config['login_passwd'] = 'imane'; // user password http auth use
