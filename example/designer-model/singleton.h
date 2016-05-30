dae5ecf7beffe3b370be468023df978e766da871
imane <imane@imane-VirtualBox.(none)>
2016-05-31 01:45:30 +0800

    add code practice

---
+++ example/designer-model/singleton.h
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  singleton.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016年05月31日 01时30分13秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  imane (), imanecr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef __SINGLETON_H__
+#define __SINGLETON_H__
+#include <iostream>
+
+class CSingleton
+{
+	public:
+		static CSingleton *getInstance();
+	private:
+		static CSingleton* m_pInstance;
+};
+
+#endif
