6ea60a2d5ff5e91f0379aa6f9da263f4af1e154d
ime <ime@ime.(none)>
2016-07-01 14:43:23 +0800

    add game

---
+++ gamecenter/static/h5/htdocs/h5game/games/liankan/js/config.main.js
@@ -0,0 +1,2 @@
+dataForWeixin.appId = "wx8820cdf5db680ffa";
+dataForWeixin.url = "http://game.jvziyouxi.com/"+_con["num"]+"/";
---
+++ gamecenter/static/h5/htdocs/h5game2/games/liankan/js/config.main.js
@@ -0,0 +1,2 @@
+dataForWeixin.appId = "wx8820cdf5db680ffa";
+dataForWeixin.url = "http://www.51tingweikeji.com/youxi/"+_con["num"]+"/";
